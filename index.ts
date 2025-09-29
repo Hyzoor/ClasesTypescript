7cc8d8f8ca413e1d56dc69d1c78ca4dd909257f2
Fernando <mkiii@hotmail.es>
2025-09-29 17:00:22 +0200

    axios install

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
 
-const miVariable: string = "Hola kk";
-console.log(miVariable);
+
+fetch("https://rickandmortyapi.com/api/character/1").then((response) => {
+    
+    const data = response.json();
+    data.then((data) => {
+        console.log(data);
+    })
+    
+}).catch((error) => {
+    console.log("Error", error);
+}).finally(() => {
+    console.log("Fin de la peticion");
+});
