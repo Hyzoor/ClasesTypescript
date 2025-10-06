561b1f5bbff35f2f784c5c67d8043eaec0f1aadb
Fernando <mkiii@hotmail.es>
2025-10-06 17:29:57 +0200

    async class

---
+++ Async/src/index.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const getMultipleCharactersName = async (ids: number[]) => {
+
+   
+    const charactersPromises = ids.map(async (x) => {
+        
+        return (await axios.get("https://rickandmortyapi.com/api/character/" + x)).data;
+
+    });
+
+    const characters = await Promise.all(charactersPromises);
+
+    const charactersNames: string[] = characters.map((x) => {
+        return x.name;
+    })
+
+    return charactersNames;
+
+
+}
+
+console.log(await getMultipleCharactersName([1,2,3]));
+
+// Promise.allSettled ver
